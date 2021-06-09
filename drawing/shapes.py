29e47067a73bc4f30260530d029bbb15ea366534
Goncalo <gmlucas98@gmail.com>
2021-06-09 12:11:49 +0100

    Initial Commit

---
+++ drawing/shapes.py
@@ -0,0 +1,10 @@
+def oval():
+    print("oval")
+
+def square(num):
+    if num == 2:
+        print("quadrado")
+    if num == 4:
+        print("rectangulo")
+    else:
+        print("enganou-se")
